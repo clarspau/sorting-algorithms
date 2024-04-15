7e7421703021b86fd00e901b2893c63262b5b03d
clarspau <125636195+clarspau@users.noreply.github.com>
2024-04-14 20:44:40 -0700

    pivot and quickSort

--- quick.js
+++ quick.js
@@ -1,14 +1,36 @@
-/*
-pivot accepts an array, starting index, and ending index
-You can assume the pivot is always the first element
-*/
+function pivot(arr, start = 0, end = arr.length - 1) {
+     const swap = (arr, idx1, idx2) => {
+          // Swap the elements in the array
+          [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+     };
 
-function pivot(){}
+     let pivot = arr[start];
+     let swapIdx = start;
 
-/*
-quickSort accepts an array, left index, and right index
-*/
+     // Loop through the array and swap the pivot with the first element that is less than the pivot
+     for (let i = start + 1; i <= end; i++) {
+          // If the pivot is greater than the current element, increment the swapIdx and swap the current element with the element at the swapIdx
+          if (pivot > arr[i]) {
+               swapIdx++;
+               swap(arr, swapIdx, i);
+          }
+     }
 
-function quickSort() {}
+     // Swap the pivot with the element at the swapIdx
+     swap(arr, start, swapIdx);
+     return swapIdx;
+}
 
-module.exports = quickSort;
+function quickSort(arr, left = 0, right = arr.length - 1) {
+     // Base case: If the left pointer is less than the right pointer
+     if (left < right) {
+          // Get the pivot index
+          let pivotIndex = pivot(arr, left, right);
+          // Recursively call the quickSort function on the left and right side of the pivot
+          quickSort(arr, left, pivotIndex - 1);
+          quickSort(arr, pivotIndex + 1, right);
+     }
+     return arr;
+}
+
+module.exports = { pivot, quickSort };
